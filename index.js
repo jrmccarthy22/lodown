faa756ce71382e9a8fdac063267e186497e432de
Ubuntu <ubuntu@ip-172-31-10-10.ec2.internal>
2021-03-24 00:34:20 +0000

    add work

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-'use strict';
-
-// YOU KNOW WHAT TO DO //
 
 /**
  * each: Designed to loop over a collection, Array or Object, and applies the 
@@ -25,16 +22,18 @@ module.exports.each = each;
 
 /*
     identity: Returns value unchanged
-    
+    param: value being identified
 */
 
 function identity(value) {
     return value;
 }
+module.exports.identity = identity;
 
 
 /*
     typeOf: Returns the type of <value> as a string
+    param: value being typed
 */
 
 function typeOf(value) {
@@ -58,13 +57,16 @@ function typeOf(value) {
         return "undefined";
     }
 }
+module.exports.typeOf = typeOf;
 
 /*
-    first: If given array is not an array, return []; if <number> is not given or not a number,
-           return just the first element in <array>
-           otherwise, return the first <number> items of <array>
+    first: should return first {num}# of elements in an array, 
+        but if given array is not an array or num is negative, return [];
+        if {num} is not given or not a number,
+           return just the first element in array
     param {array}: the array we are using 
-    param {num}: the number of items in the array we are returning, if num is a positive number less than thr array length
+    param {num}: the number of items in the array we are returning, 
+    if num is a positive number less than the array length
 */
 
 function first(array, num) {
@@ -83,13 +85,16 @@ function first(array, num) {
     }
     
 }
+module.exports.first = first;
 
 /*
-    last: If given array is not an array, return []; if <number> is not given or not a number,
-        return just the last element in <array>
-        Otherwise, return the last <number> items of <array>
+    last: should return last {num}# of elements in an array,
+        but if given array is not an array or num is negative, return []; 
+        if {num} is not given or not a number,
+        return just the last element in array
     param {array}: array used in function 
-    param {num}: the number of items in the array we are returning, if num is a positive number less than thr array length
+    param {num}: the number of items in the array we are returning,
+    if num is a positive number less than the array length
 */
 
 function last(array, num) {
@@ -107,9 +112,13 @@ function last(array, num) {
         return result;
     }
 }
+module.exports.last = last;
 
 /*
-    indexOf:
+    indexOf: returns the index of {value} that is its first occurence in {array}
+        if value isn't in array, return -1
+    param {array}: array we are using
+    param {value}: value we are looking for in array to find the index of
 */
 
 function indexOf(array, value) {
@@ -122,8 +131,13 @@ function indexOf(array, value) {
     }
 
 }
+module.exports.indexOf = indexOf;
+
 /*
-    contains:
+    contains: returns true if the value is one of the elements in the array
+              returns false otherwise
+    param {array}: array we are testing
+    param {value}: value we are searching for
 */  
 
 function contains(array, value) {
@@ -135,17 +149,25 @@ function contains(array, value) {
         }
     
 }
+module.exports.contains = contains;
 
 /*  
-    unique:
+    unique: returns new array of all elements in the array with duplicates removed
+  
+    param {array}: collection we are filtering
 */
 
 function unique(array) {
     return array.filter((value, index) => array.indexOf(value) === index);
 }
+module.exports.unique = unique;
 
  /*  
-    filter:
+    filter: returns array of elements in {array} for which the function call returned true
+    
+    param {array}: array to be filtered
+    param {test}: function to be applied to every element in array that should return a boolean
+                has arguments of (element, index, array)
 */
 
 function filter(array, test) {
@@ -158,9 +180,14 @@ function filter(array, test) {
         }
     } return newArray;
 }
+module.exports.filter = filter;
 
 /*
-    reject:
+    reject: inverse of filter;
+            returns array of elements in {array} for which the function call returned false
+    param {array}: array to be filtered
+    param {test}: function to be applied to every element in array that should return a boolean
+                has arguments of (element, index, array)
 */
 
 function reject(array, test) {
@@ -172,9 +199,16 @@ function reject(array, test) {
         }
     } return newArray;
 }
+module.exports.reject = reject;
 
 /*
-    partition:
+    partition: should call {test} function for each element in {array} with the arguments:
+*       element, key, {array}
+*   should return an array that is made up of 2 sub arrays:
+*       0) An array that contains all the values for which {test}function returned something truthy
+*       1) An array that contains all the values for which {test}function returned something falsy
+    param {array}: array being tested
+    param {test}: function called on each element of array
 */
 
 function partition(array, test) {
@@ -189,11 +223,15 @@ function partition(array, test) {
         }
     } return [truthy, falsey];
 }
-
+module.exports.partition = partition;
 /*
-    map:
+    map: calls {test}function for each element in collection, using following arguments:
+*        if {list} is an array: (element, index, {list})
+*        if {list} is an object: (value, key, {list})
+        returns new array with values of each function call 
+    param {list}: array or object
+    param {test}: function being called on each element in collection
 */
-
 
 function map(list, test) {
     var newArray = [];
@@ -210,9 +248,13 @@ function map(list, test) {
     }
     return newArray;
 }
+module.exports.map = map;
 
 /*
-    pluck:
+    pluck: using _.map, return an array of values found under the {property} key for each object in {array}
+
+    param {array}: array of objects 
+    param {property}: keyname of value we want to return 
 */
 
 function pluck(array, property) {
@@ -220,9 +262,15 @@ function pluck(array, property) {
         return element[property]; 
     });
 }
+module.exports.pluck = pluck;
 
 /*
-    every:
+    every: use _.each to call {test}function on every element of the collection
+        if every function call is true, returns true
+        if at least one returns false, returns false
+        if function is not provided, test truthiness of values in the collection w same rules as above
+    param {collection}: array or object 
+    param {test}: function being called on each element w arguments (element, index/key, collection)
 */
 
 function every(collection, test) {
@@ -241,19 +289,72 @@ function every(collection, test) {
         })
     } return bool;
 }
+module.exports.every = every;
 
 /*
-    some:
+    some:use _.each to call {test}function on every element of the collection
+        if every function call is false, returns false
+        if at least one returns true, returns true
+        if function is not provided, test truthiness of values in the collection w same rules as above
+    
+    param {collection}:array or object
+    param {test}: function being called on each element with arguments (element, index/key, collection)
 */
 
-
+function some(collection, test) {
+     var bool = false;
+    if (typeof test === 'function') {
+        _.each(collection, function(val, i, col) {
+            if (test(val, i, col) === true) {
+                bool = true;
+            }
+        })
+    } else {
+        _.each(collection, function(val, i, col) {
+            if (val === true) {
+                bool = true;
+            }
+        })
+    } return bool;
+}
+module.exports.some = some;
 
 /*
-    reduce:
+    reduce: should call {func}tion on every element of array
+            if no seed is given, use frst element of array as seed and continue to the rest
+            return value of final function call 
+    param {array}: array being used 
+    param {func}: function being called on each element of array w arguments (seed, element, index)
+    param {seed}: starting point/starting value, or an empty array/object; this gets added to and returned at the end 
 */
 
-
+function reduce(array, func, seed) {
+    
+    if (seed !== undefined) {
+        for (var i = 0; i < array.length; i++) {
+            seed = func(seed, array[i], i);
+        } return seed;
+    } else {
+        seed = array[0];
+        for (var i = 1; i < array.length; i++) {
+            seed = func(seed, array[i], i);
+        } return seed;
+    }
+    
+}
+module.exports.reduce = reduce;
 
 /*
-    extend:
+    extend: should copy properties into object1 with properties of object2
+        includes any possible objects that can be passed as an argument
+        returns updated object1
+    param {object1}: target object
+    param {object2 and more}: objects whose properties are being copied into object1
 */
+
+function extend(object1, object2) {
+    
+    Object.assign(object1, ...arguments);
+    return object1;
+}
+module.exports.extend = extend;
